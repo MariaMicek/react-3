d9529aca1f9f0fee484bf2829d4102306b3c7d88
Maria Micek <mmicek16@wp.pl>
2019-04-03 14:54:43 +0200

    life cycle methods and order of calling, router, setInterval with life cycle methods

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react'
 import Counter from './Counter'
 import TypeName from './TypeName'
 import './index.css'
-import LifeCycle from './LifeCycle/LifeCycle';
+import LifeCycle from './LifeCycle/LifeCycle'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Counter number={0}/>
+      <Counter number={0} />
       <TypeName />
-      <LifeCycle />
+
+      <BrowserRouter>
+        <Route path={'/life-cycle'} component={LifeCycle} />
+        <ul>
+          <li>
+            <Link to={'/'}>Home</Link>
+          </li>
+          <li>
+            <Link to={'/life-cycle'}>LifeCycle</Link>
+          </li>
+        </ul>
+      </BrowserRouter>
     </div>
   )
 }
--- src/LifeCycle/LifeCycle.js
+++ src/LifeCycle/LifeCycle.js
@@ -5,25 +5,68 @@ class LifeCycle extends React.Component {
         super()
 
         this.state = {
-            number: 0
+            number: 0,
+            time: (new Date()).toLocaleString()
         }
+        this.intervalID = null
+
         console.log('constructor')
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount')
     }
 
     componentDidMount() {
         console.log('componentDidMount')
+        console.log('--------------------')
+
+        this.intervalID = setInterval(
+            () => this.setState({time: (new Date()).toLocaleString()}),
+            1000
+        )
+    }
+
+    componentWillReceiveProps(nextProps){           //metoda wycofana
+        console.log('componentWillReceiveProps')
+    }
+
+    componentWillUpdate(){                  //metoda wycofana
+        console.log('componentWillUpdate')
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('shouldComponentUpdate')
+        console.log("Current props: ", this.props)
+        console.log("Next props: ", nextProps)
+        console.log("Current state: ", this.state)
+        console.log("Next state: ", nextState)
+
+        return true
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+        clearInterval(this.intervalID)
     }
 
     render() {
-        console.log('render')           //na początku wywołują się wszystkie metody, po kliknięciu na button wywołuje się tylko render!
+        console.log('render')          
 
         return (
             <div>
-                <button
-                    onClick={() => this.setState({ number: this.state.number + 1 })}
-                >
-                    CHANGE STATE
-                </button>
+                <div>{this.state.time}</div>
+                <div>
+                    <button
+                        onClick={() => this.setState({ number: this.state.number + 1 })}
+                    >
+                        CHANGE STATE
+                    </button>
+                </div>
             </div>
         )
     }
